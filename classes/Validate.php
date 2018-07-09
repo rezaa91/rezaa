dc838735f2acce3a8bc6a76e10b42faefb2a7e1c
rezaa91 <rezaa91@hotmail.co.uk>
2018-07-09 18:11:07 +0100

    validate user inputted data

---
+++ classes/Validate.php
@@ -0,0 +1,20 @@
+<?php
+
+//validate user input data
+class Validate{
+
+    function isEmail($email){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return true;
+        }
+    }
+
+    function isStr($str){
+        if(!empty($str)){
+            return htmlentities($str);
+        }
+    }
+
+}
+
+?>
