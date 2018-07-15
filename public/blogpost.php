4cc6be0ec511dd76f7091828cc96b4759ae2998d
rezaa91 <rezaa91@hotmail.co.uk>
2018-07-15 16:53:58 +0100

    fixed bugs

--- public/blogpost.php
+++ public/blogpost.php
@@ -6,24 +6,34 @@ require('../core/init.php');
 if($_SERVER['REQUEST_METHOD'] == "GET"){
 
     $id = $_GET["id"]; //get id from url
-    
-    //get blog information from database using id from url
-    $q = "SELECT * FROM blog WHERE blog_id = :id";
-    $stmt = $pdo->prepare($q);
-    $r = $stmt->execute([':id' => $id]);
 
-    if($r){ 
-        $stmt->setFetchMode(PDO::FETCH_OBJ);
-        $blog = $stmt->fetch(); //to use in view
+    try{
+        //get blog information from database using id from url
+        $q = "SELECT * FROM blog WHERE blog_id = :id";
+        $stmt = $pdo->prepare($q);
+        $r = $stmt->execute([':id' => $id]);
+
+        if($r && $stmt->rowCount() == 1){ //if row found via id, display blog view
+            $stmt->setFetchMode(PDO::FETCH_OBJ);
+            $blog = $stmt->fetch(); //to use in view
 
 
-        $page_title = $blog->title; //set title to blog title
+            $page_title = $blog->title; //set title to blog title
 
-        //include views
-        include('../includes/header.inc.php');
-        include('../views/blogpost.html');
-        include('../includes/footer.inc.php');
+            //include views
+            include('../includes/header.inc.php');
+            include('../views/blogpost.html');
+            include('../includes/footer.inc.php');
+
+        }else{ //if page does not exist...
+            throw new Exception('This page does not exist.');
+        }
+
+    }catch(Exception $e){
+        display_error_page($e);
     }
+    
+    
 
 }else{
     header('location: index.php');
