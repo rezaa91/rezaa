76e2ee6f7cd49fc6227e9ed20087817e6abfd254
rezaa91 <rezaa91@hotmail.co.uk>
2018-07-15 16:51:52 +0100

    check file exists before including files

--- functions/display_error_page.php
+++ functions/display_error_page.php
@@ -5,9 +5,13 @@
 
 function display_error_page($error){
     $page_title = "Error!";
-    include('../includes/header.inc.php');
-    include('../views/error_page.html');
-    include('../includes/footer.inc.php');
+
+    //include error pages if one directory deep
+    if(file_exists('../includes/header.inc.php')){
+        include('../includes/header.inc.php');
+        include('../views/error_page.html');
+        include('../includes/footer.inc.php');
+    }
 }
 
 
