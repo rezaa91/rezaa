82a4853494394a19a9d41d69ac0d77f402e790a0
rezaa91 <rezaa91@hotmail.co.uk>
2018-07-16 21:59:21 +0100

    added CRUD - edit functionality

--- public/edit.php
+++ public/edit.php
@@ -65,7 +65,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
         }
 
     }else{
-        header('location: logout.php');
+        header('location: index.php');
     }
 
     include('../includes/footer.inc.php');
