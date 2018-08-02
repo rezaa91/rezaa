b523af2299bce33864e22a91cc807408cb7ae94f
rezaa91 <rezaa91@hotmail.co.uk>
2018-08-02 20:48:23 +0100

    created email template

---
+++ includes/email.php
@@ -0,0 +1,18 @@
+<?php
+
+$body = "
+<div style='width:100%; border: 1px solid black; box-shadow: 1px 2px 3px grey;'>
+
+    <div style='padding: 10px 5px; background-color: red; color: white; font-weight: bold; text-align:center;'>
+        <h2 style='font-size: 32px'>Rezaa Tutorials</h2>
+    </div>
+
+    <div style='text-align:center;'>
+        <p style='font-weight: bold; font-size: 18px; margin-top: 30px;'>Thank you for subscribing to Rezaa Tutorials.</p>
+        <p style='margin-top: 10px;'>We deliver 1x newsletters per month. We hope you enjoy them.</p>
+        <a style='color: white; background-color: red; padding: 10px; border-radius: 5px; margin-top: 20px; box-shadow: 1px 2px 3px grey;'>VISIT WEBSITE</a>
+    </div>
+    
+</div>";
+
+?>
