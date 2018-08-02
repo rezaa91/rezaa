79795f6e3cc3038f772beee5988945cc44468ed1
rezaa91 <rezaa91@hotmail.co.uk>
2018-08-02 20:48:05 +0100

    send email through script

--- public/index.php
+++ public/index.php
@@ -4,8 +4,10 @@
 require('../core/init.php');
 
 
+//insert users email address in to phpmyadmin table
 //Check whether email subscription form posted and handle
 if($_SERVER['REQUEST_METHOD'] == "POST"){
+
     //validate email address
     $validate = new Validate(); //new Validate instance obj
     $email = $validate->isEmail($_POST['email']); //check if valid email before posting to database
@@ -13,23 +15,32 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     //insert email into database if valid and not already submitted
     try{
         if($email){
+            $email = $_POST['email'];
             //check whether email is already in database
             $q = "SELECT email FROM email_marketing WHERE email = :email";
             $stmt = $pdo->prepare($q);
-            $r = $stmt->execute([':email' => $_POST['email']]);
+            $r = $stmt->execute([':email' => $email]);
 
             if($r && $stmt->rowCount() == 0){ //if inputted email does not exist already in table, add to database
-                $q = "INSERT INTO email_marketing(email) VALUES(:email)";
-                $stmt = $pdo->prepare($q);
-                $r = $stmt->execute([':email' => $_POST['email']]); //insert email in to database
+                //require email/smtp configuration
+                require('../core/smtp.php'); //send email
 
-                if($r){ //redirect user to homepage with cookie specifying successful message - if successfully inputted in to database
-                    $_SESSION['success'] = "You have successfully subscribed for newsletters and updates"; //show successful message on index page after redirect
-                    header('location: index.php');
-                    exit();
-                }else{ //if error inserting data - inform user
-                    throw new Exception('Sorry, something went wrong. Please try again');
+                //inupt data in to database if email successfully sent
+                if($mail->send()){
+                    $q = "INSERT INTO email_marketing(email) VALUES(:email)";
+                    $stmt = $pdo->prepare($q);
+                    $r = $stmt->execute([':email' => $email]); //insert email in to database
+
+                    if($r){ //redirect user to homepage with cookie specifying successful message - if successfully inputted in to database
+                        $_SESSION['success'] = "You have successfully subscribed for newsletters and updates"; //show successful message on index page after redirect
+                        header('location: index.php');
+                        exit();
+    
+                    }else{ //if error inserting data - inform user
+                        throw new Exception('Sorry, something went wrong. Please try again');
+                    }
                 }
+                
 
             }else{ //if user inputted email already exists, inform user
                 throw new Exception('The email supplied is already subscribed');
@@ -41,7 +52,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         }
 
     }catch(Exception $e){
-        echo $e->getMessage();
+        $_SESSION['failure'] = $e->getMessage();
     }
 
 }
