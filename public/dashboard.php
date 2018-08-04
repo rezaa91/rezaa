cb0269f5f3566df47ed7db07aa7ecb374341569a
rezaa91 <rezaa91@hotmail.co.uk>
2018-08-04 22:50:48 +0100

    redirect user to index with error message if accessed when not logged in

--- public/dashboard.php
+++ public/dashboard.php
@@ -4,5 +4,13 @@
  * This script shows dashboard data to administrators
  */
 
+ require '../core/init.php';
+
+ //redirect user if page accessed in error
+ if(!$logged_in){
+     $_SESSION['failure'] = 'Unauthorised access!';
+     header('location: index.php');
+     exit();
+ }
 
 ?>
