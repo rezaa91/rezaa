bc9b3204611ce39cd3b8ba13ac8b075495d57970
rezaa91 <rezaa91@hotmail.co.uk>
2018-08-09 18:58:06 +0100

    created initial email callback

--- includes/email.php
+++ includes/email.php
@@ -1,18 +1,45 @@
 <?php
 
 $body = "
-<div style='width:100%; border: 1px solid black; box-shadow: 1px 2px 3px grey;'>
+<div style='width: 90%; margin: auto; padding: 20px;'>
 
-    <div style='padding: 10px 5px; background-color: red; color: white; font-weight: bold; text-align:center;'>
-        <h2 style='font-size: 32px'>Rezaa Tutorials</h2>
+    <div style='text-align: center; border-bottom: 2px solid grey;'>
+        <h1 style='font-family: sans-serif; color: #B30B0B;'>Rezaa</h1>
+        <p style='font-size: 18px; font-weight: bold;'>Follow my journey and learn along the way</p>
     </div>
 
-    <div style='text-align:center;'>
-        <p style='font-weight: bold; font-size: 18px; margin-top: 30px;'>Thank you for subscribing to Rezaa Tutorials.</p>
-        <p style='margin-top: 10px;'>We deliver 1x newsletters per month. We hope you enjoy them.</p>
-        <a style='color: white; background-color: red; padding: 10px; border-radius: 5px; margin-top: 20px; box-shadow: 1px 2px 3px grey;'>VISIT WEBSITE</a>
+
+    <div style='text-align: center; margin-bottom: 50px; font-size: 18px;'>
+        <p style=' color: #989898; font-weight: bold; letter-spacing: 1.6px; line-height: 26px;'><span style='font-size: 18px; color: #272424'>Thank you for subscribing.</span><br>Newsletter issue #1 will be sent out Monday 5th Novemeber. Following issues will be released the first Monday every month thereafter. In the meantime, why don't you get acquainted with my website. See below for some of the content you can enjoy right now...</p>
     </div>
-    
+
+
+    <div style='width: 80%; margin:0 auto 50px; text-align: center;'>
+        <h2 style='color:#989898;'>Projects</h2>
+        <p style='font-size: 18px;'>View some of my recent projects... Right now you are able to view freelancing website which was created using OOP PHP &amp; JavaScript.</p>
+        <a href='http://rezaa.com' style='font-style: italic; color: #B30B0B; font-family: sans-serif; font-size: 18px;'>view more...</a>
+    </div>
+
+    <div style='width: 80%; margin:0 auto 50px; text-align: center;'>
+        <h2 style='color:#989898;'>Blogs</h2>
+        <p style='font-size: 18px;'>Follow my story as I progress through web development. Read how I began my journey, you may also learn a trick or two along the way.</p>
+        <a href='http://rezaa.com' style='font-style: italic; color: #B30B0B; font-family: sans-serif; font-size: 18px;'>view more...</a>
+    </div>
+
+    <div style='width: 80%; margin:0 auto 70px; text-align: center;'>
+        <h2 style='color:#989898;'>Tutorials</h2>
+        <p style='font-size: 18px;'>HTML5 and CSS3 tutorials are currently uploaded on to my website. JavaScript tutorials will follow shortly. These are a brilliant resource you can use to get your feet wet if you are new to web development.</p>
+        <a href='http://rezaa.com' style='font-style: italic; color: #B30B0B; font-family: sans-serif; font-size: 18px;'>view more...</a>
+    </div>
+
+
+    <div style='width: 100%; margin: auto; margin-bottom: 20px; text-align: center; border-top: 2px solid grey; border-bottom: 1px solid black; padding: 10px; background-color: #B30B0B'>
+        <h3 style='margin:0 0 10px; padding: 0; color: #f9f9f9'>Follow me on social media...</h3>
+        <a href='https://twitter.com/_rezaa91' target='_blank' style='color:#272424; margin: 0 10px;'>Twitter</a>
+        <a href='https://instagram.com/_rezaa91' target='_blank' style='color:#272424; margin: 0 10px;'>Instagram</a>
+        <a href='https://www.youtube.com/channel/UCGPpiPgghR8uiaIIJuvKZtg' target='_blank' style='color:#272424; margin: 0 10px;'>Youtube</a>
+    </div>
+
 </div>";
 
 ?>
